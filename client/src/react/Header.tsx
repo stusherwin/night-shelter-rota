73e6b933646987ffc00c6ba83ffef958cf1f5af6
Stu Sherwin <stu.sherwin@gmail.com>
2018-02-10 18:33:42 +0000

    Header buttons

--- client/src/react/Header.tsx
+++ client/src/react/Header.tsx
@@ -2,34 +2,106 @@ import * as React from 'react';
 import { Vol } from './Types'
 import { MessageBubble, MessageBubbleProps, MessageBubbleAction } from './MessageBubble'
 
+export type VolDetailsState = 'NotEditing' 
+                            | 'EditingNewVol'
+                            | 'EditingCurrentVol'
+
 export interface HeaderProps { reqInProgress: boolean
                              , errorMessage: MessageBubbleProps
                              , vols: Vol[]
                              , initialDataLoaded: boolean
                              , errorMessageAction: (action: MessageBubbleAction) => void
+                             , changeCurrentVol: (vol: Vol | null) => void
+                             , editCurrentVol: () => void
+                             , editNewVol: () => void
                              }
-export interface HeaderState {}
+
+export interface HeaderState { volDetailsState: VolDetailsState
+                             , currentVol: Vol | null
+                             }
 
 export class Header extends React.Component<HeaderProps, HeaderState> {
-    render() {
-      if(!this.props.initialDataLoaded) {
-        return <div className="header initial-data-loading">
-                 <StatusIcon reqInProgress={this.props.reqInProgress}
-                             errorMessage={this.props.errorMessage}
-                             action={this.props.errorMessageAction}>
-                 </StatusIcon>
-                 <h2>Night Shelter Rota</h2>
-               </div>
-      } else {
-        return <div className="header">
-                 <StatusIcon reqInProgress={this.props.reqInProgress}
-                             errorMessage={this.props.errorMessage}
-                             action={this.props.errorMessageAction}>
-                 </StatusIcon>
-                 <h2>Night Shelter Rota for {this.props.vols[0].name}</h2>
-               </div>
-      }
+  constructor(props: HeaderProps) {
+    super(props)
+    this.state = { volDetailsState: 'NotEditing', currentVol: null }
+  }
+ 
+  render() {
+    if(!this.props.initialDataLoaded) {
+      return <div className="header initial-data-loading">
+               <StatusIcon reqInProgress={this.props.reqInProgress}
+                           errorMessage={this.props.errorMessage}
+                           action={this.props.errorMessageAction}>
+               </StatusIcon>
+               <h2>Night Shelter Rota</h2>
+             </div>
+    } else {
+      return <div className="header">
+               <HeaderButtons volDetailsState={this.state.volDetailsState}
+                              currentVol={this.state.currentVol}
+                              editNewVol={this.props.editNewVol}
+                              editCurrentVol={this.props.editCurrentVol} />
+               <StatusIcon reqInProgress={this.props.reqInProgress}
+                           errorMessage={this.props.errorMessage}
+                           action={this.props.errorMessageAction}>
+               </StatusIcon>
+               <h2>Night Shelter Rota for {this.props.vols[0].name}</h2>
+             </div>
     }
+  }
+}
+
+function HeaderButtons(props: { volDetailsState: VolDetailsState 
+                              , currentVol: Vol | null
+                              , editNewVol: () => void
+                              , editCurrentVol: () => void
+                              } ): JSX.Element {
+  let buttons = [];
+  if(props.volDetailsState == 'NotEditing') {
+    buttons.push(<HeaderButton buttonClassName="header-button-new"
+                               mediaClassName="media-large-screen"
+                               text="New volunteer"
+                               action={props.editNewVol} />)
+    buttons.push(<HeaderButton buttonClassName="header-button-new"
+                               mediaClassName="media-larger-screen media-medium-screen"
+                               text="New"
+                               action={props.editNewVol} />)
+    buttons.push(<HeaderButton buttonClassName="header-button-new"
+                               mediaClassName="media-small-screen"
+                               text={null}
+                               action={props.editNewVol} />)
+
+    if(props.currentVol != null) {
+      buttons.push(<HeaderButton buttonClassName="header-button-edit"
+                                 mediaClassName="media-large-screen"
+                                 text="Edit volunteer details"
+                                 action={props.editCurrentVol} />)
+      buttons.push(<HeaderButton buttonClassName="header-button-edit"
+                                 mediaClassName="media-larger-screen media-medium-screen"
+                                 text="Edit"
+                                 action={props.editCurrentVol} />)
+      buttons.push(<HeaderButton buttonClassName="header-button-edit"
+                                 mediaClassName="media-small-screen"
+                                 text={null}
+                                 action={props.editCurrentVol} />)
+    }
+  }
+  
+  return <div className="header-buttons">
+           {buttons}
+         </div>
+}
+
+function HeaderButton(props: { buttonClassName: string
+                             , mediaClassName: string
+                             , text: string | null
+                             , action: () => void
+                             } ): JSX.Element | null {
+  return <button className={`ui button header-button ${props.buttonClassName} ${props.mediaClassName}`}
+                 onClick={e => {e.preventDefault(); props.action(); }}>
+           <i className="icon icon-add"></i>
+           {props.text}
+         </button>
 }
 
 class StatusIcon extends React.Component<{ reqInProgress: boolean
@@ -39,9 +111,9 @@ class StatusIcon extends React.Component<{ reqInProgress: boolean
   render() {
     return <div className="header-status">
              <i className={this.iconType()}
-                onClick={ e => { e.preventDefault(); this.props.action('ToggleFixed'); }}
-                onMouseOver={ e => { e.preventDefault(); this.props.action('ShowTransitory'); }}
-                onMouseLeave={ e => { e.preventDefault(); this.props.action('HideTransitory'); }}>
+                onClick={e => { e.preventDefault(); this.props.action('ToggleFixed'); }}
+                onMouseOver={e => { e.preventDefault(); this.props.action('ShowTransitory'); }}
+                onMouseLeave={e => { e.preventDefault(); this.props.action('HideTransitory'); }}>
              </i>
              <MessageBubble {...this.props.errorMessage}
                             action={this.props.action}>
--- client/src/react/ShelterRota.tsx
+++ client/src/react/ShelterRota.tsx
@@ -12,40 +12,51 @@ export interface ShelterRotaState { vols: Vol[]
                                   }
 
 export class ShelterRota extends React.Component<ShelterRotaProps, ShelterRotaState> {
-    constructor(props: ShelterRotaProps) {
-      super(props)
-      this.state = { vols: [], reqInProgress: true, errorMessage: new MessageBubbleProps(), initialDataLoaded: false }
-      this.errorMessageAction = this.errorMessageAction.bind(this)
-    }
+  constructor(props: ShelterRotaProps) {
+    super(props)
+    this.state = { vols: [], reqInProgress: true, errorMessage: new MessageBubbleProps(), initialDataLoaded: false }
+  }
 
-    componentDidMount() {
-      Promise.all([ServerApi.vols()])
-        .then(results => {
-          console.log(results);
-          this.setState({ reqInProgress: false, vols: results[0], initialDataLoaded: true })
-        })
-        .catch(err => {
-          let apiError = err as ApiError
-          console.log(err)
-          this.setState({ reqInProgress: false, errorMessage: this.state.errorMessage.setMessage({ header: apiError.error, body: apiError.message, position: 'under', icon: 'warning'}) })
-        })
-    }
+  componentDidMount() {
+    Promise.all([ServerApi.vols()])
+      .then(results => {
+        console.log(results);
+        this.setState({ reqInProgress: false, vols: results[0], initialDataLoaded: true })
+      })
+      .catch(err => {
+        let apiError = err as ApiError
+        console.log(err)
+        this.setState({ reqInProgress: false, errorMessage: this.state.errorMessage.setMessage({ header: apiError.error, body: apiError.message, position: 'under', icon: 'warning'}) })
+      })
+  }
 
-    render() {
-      return <div>
-               <Header reqInProgress={this.state.reqInProgress}
-                       errorMessage={this.state.errorMessage}
-                       initialDataLoaded={this.state.initialDataLoaded}
-                       vols={this.state.vols}
-                       errorMessageAction={this.errorMessageAction}>
-               </Header>
-               <div className="container">
-                 <p>Some stuff here...</p>
-               </div>
+  render() {
+    return <div>
+             <Header reqInProgress={this.state.reqInProgress}
+                     errorMessage={this.state.errorMessage}
+                     initialDataLoaded={this.state.initialDataLoaded}
+                     vols={this.state.vols}
+                     errorMessageAction={this.errorMessageAction.bind(this)}
+                     changeCurrentVol={this.changeCurrentVol.bind(this)}
+                     editNewVol={this.editNewVol.bind(this)}
+                     editCurrentVol={this.editCurrentVol.bind(this)}>
+             </Header>
+             <div className="container">
+               <p>Some stuff here...</p>
              </div>
-    }
+           </div>
+  }
 
-    errorMessageAction(action: MessageBubbleAction) {
-      this.setState({errorMessage: this.state.errorMessage.afterAction(action)})
-    }
+  errorMessageAction(action: MessageBubbleAction) {
+    this.setState({errorMessage: this.state.errorMessage.afterAction(action)})
+  }
+
+  changeCurrentVol(vol: Vol | null) {
+  }
+
+  editNewVol() {
+  }
+
+  editCurrentVol() {
+  }
 }
